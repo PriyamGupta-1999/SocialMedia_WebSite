cad8560e491023d9b3f74a13e56d1e7371f41d4e
Priyam Gupta <priyamgupta@Priyams-MacBook-Pro.local>
2022-01-27 23:42:16 +0530

    add a controller and router for home

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const { kMaxLength } = require('buffer');
+const express=require('express');
+const { append } = require('vary');
+const router =express.Router();
+
+
+const homecontroller=require('../controllers/home_controller');
+console.log('ruter loaded');
+
+router.get('/',homecontroller.home);
+module.exports=router;
