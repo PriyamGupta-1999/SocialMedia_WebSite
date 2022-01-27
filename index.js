f0d79bb9ca9e5b63657b0b921a5141def0af1e65
Priyam Gupta <priyamgupta@Priyams-MacBook-Pro.local>
2022-01-28 01:55:15 +0530

    Create a view for home

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express=require('express');
 const app=express();
 const port=8000;
 
+//setting view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 
 //uisng router of routes
 app.use('/',require('./routes'));
