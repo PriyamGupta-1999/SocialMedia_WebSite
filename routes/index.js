0b8ce5119d8488acac9396c7a7c6964933da889b
Priyam Gupta <priyamgupta@Priyams-MacBook-Pro.local>
2022-01-28 03:56:46 +0530

    Setting up User Schema

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,7 @@ const homecontroller=require('../controllers/home_controller');
 console.log('ruter loaded');
 
 router.get('/',homecontroller.home);
+
+//whenver something for user comes use this and got to users.js
 router.use('/users',require('./users'));
 module.exports=router;
