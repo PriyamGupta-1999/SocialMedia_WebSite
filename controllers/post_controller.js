973216ffba23b739416c7ef81954da4082aa90d4
Priyam Gupta <priyamgupta@Priyams-MacBook-Pro.local>
2022-02-04 02:27:48 +0530

    Deleting a Post (Authorized)

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,5 +1,5 @@
 const Post = require("../models/post");
-
+const Comment=require('../models/comment');
 module.exports.post=function(req,res){
     return res.end('<h1> user posts</h1>');
 }
@@ -15,5 +15,22 @@ module.exports.create=function(req,res){
         }
 
         return res.redirect('back');
+    })
+}
+
+//DELETING THE POSTS
+module.exports.destroy = function(req,res){
+    // console.log('11')
+    Post.findById(req.params.id, function(err, post){
+        //.id is used instead of ._id -> used to convert object to string
+        if(post.user==req.user.id){
+            post.remove();
+            //function given by mongo to delete many comments with paraameter id 
+            Comment.deleteMany({post: req.params.id}, function(err){
+                return res.redirect('back');
+            })
+        }else{
+            return res.redirect('back');
+        }
     })
 }
--- routes/posts.js
+++ routes/posts.js
@@ -3,6 +3,8 @@ const router=express.Router();
 const passport=require('../config/passport-local-strategy');
 const postController=require('../controllers/post_controller');
 
+//apply checkAuthentication, so that only signed user can only create the form 
 router.post('/create',passport.checkAuthentication,postController.create);
 
+router.get('/destroy/:id', passport.checkAuthentication,postController.destroy);
 module.exports=router;
