2f9a0a715f5776a131d73513e49e613b500ddd53
Priyam Gupta <priyamgupta@Priyams-MacBook-Pro.local>
2022-02-05 01:25:42 +0530

    adding async to home controller

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,49 +1,32 @@
 const Post = require("../models/post");
 const User = require("../models/user");
+//async function for the process to accompolish 
 
-module.exports.home=function(req,res){
-
-    // console.log(req.cookies);
-
-    // just display user id 
-
-    // Post.find({},function(err,posts){
-    //     return res.render('home',{
-    //         title: "Codiel | Home",
-    //         posts: posts
-    //     })
-    // })
-
-    //display whole as user POPULATING THE USER
-    
-    //for only populating user 
-    // Post.find({}).populate('user').exec(function(err,posts){
-    //     return res.render('home', {
-    //         title: "Codiel | Home",
-    //         posts: posts
-    //     })
-    // })
-
-    //for populatingj user commnetns and user that commentted on the comment 
-    Post.find({})
+module.exports.home= async function(req,res){
+    //await to mention complete this and then go ahead 
+    try{
+    let posts= await Post.find({})
     .populate('user')
     .populate({
         path: 'Comments',
         populate: {
             path: 'user'
         }
-    })
-    .exec(function(err,posts){
+    });
+    
 
         //query to find all the users 
-        User.find({},function(err,users){
-            return res.render('home', {
-                title: "Codiel | Home",
-                posts: posts,
-                all_users: users
-            })
-        })
+    let users= await User.find({});
         
+    return res.render('home', {
+        title: "Codiel | Home",
+        posts: posts,
+        all_users: users
     })
+    }catch(err){
+        console.log('Error',err);
+    } 
+        
+    
 }
 
