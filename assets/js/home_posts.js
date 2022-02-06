6bf562cadb53e33f353bad221df7a14f23567bfe
Priyam Gupta <priyamgupta@Priyams-MacBook-Pro.local>
2022-02-07 03:56:13 +0530

    Creating a Post :: Receiving Data

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -11,13 +11,58 @@
                 url: '/posts/create',
                 data: newPostForm.serialize(),
                 success: function(data){
-                    console.log(data);
+                    let newPost= newPostDom(data.data.post);
+                    $('#post-list-container>ul').prepend(newPost);
                 },error: function(error){
                     console.log(error.responseText);
                 }
             })
         })
     }
-
+    //creating the post in DOM
+    let newPostDom = function(post){
+        //remove all <%%> from post.ejs and make them ${} --> to operate 
+        //remove if condition to check if user is logged in or not before deleting as we can see them for now 
+        //this file will be send by above ajax request 
+        return $(`<li id="post-${post._id}">
+        <p>
+        
+           
+                <small>
+                    <a class="delete-post-button" href="/posts/destroy/${post._id}">X</a>
+                </small>    
+            
+              
+            
+            ${ post.content }
+            <br>
+            <small>
+                ${ post.user.name }
+            </small>
+        </p>
+        <!-- POST COMMENTS  -->
+        <div class="post-comments">
+            
+                
+                 <form action="/comments/create" method="POST">
+                     <input type="text" name="content" placeholder="type here to add comments..">
+                     <!-- sending hidden data  -->
+                     <input type="hidden" name="post" value="${post._id }">
+                     <input type="submit" value="Add Comment">
+    
+                 </form>
+            
+        </div>
+    
+    
+        <div id="post-comments-list">
+            <ul id="post-comments-${post._id }" >
+                
+                    
+                
+            </ul>
+        </div>
+    </li>`)
+    }
     createPost();
 }
